c454da6e76c756f317792ff4cef0f8c8f7431a71
Crowdbotics <team@crowdbotics.com>
2020-12-03 22:02:59 +0000

    Update components

--- src/features/BlankScreen1181976/screens/blank.js
+++ src/features/BlankScreen1181976/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -8,33 +8,61 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet,
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <View style={styles.View_5}>
+        <View style={styles.View_6}>
+          <Image
+            source={{
+              uri:
+                "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/images.jpeg"
+            }}
+            style={styles.Image_10}
+          />
+        </View>
+        <View style={styles.View_7}>
+          <Text style={styles.Text_12}>Sender name</Text>
+          <Text style={styles.Text_16}>Sample message content</Text>
+        </View>
+        <View style={styles.View_8}>
+          <Text style={styles.Text_20}>12:00pm</Text>
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
+  View_1: {},
+  View_5: {
+    width: "100%",
+    height: 65,
+    flexDirection: "row",
+    borderColor: "#cccccc",
+    borderBottomWidth: 2
   },
-});
+  View_6: { width: "20%", alignItems: "center" },
+  Image_10: { width: 40, height: 40, borderRadius: 40 },
+  View_7: { width: "60%" },
+  Text_12: { marginTop: 0, marginBottom: 0, fontFamily: "Lato-Bold" },
+  Text_16: { fontSize: 10, fontFamily: "Lato-Regular" },
+  View_8: { width: "20%", alignItems: "flex-end" },
+  Text_20: { fontSize: 8, fontFamily: "Lato-Regular" }
+})
